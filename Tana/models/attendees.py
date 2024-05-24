312b77e28dab22b5a8702739dd152ce42038c47f
isiji <blairisiji@gmail.com>
2024-05-24 09:43:34 +0300

    added task table

---
+++ Tana/models/attendees.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Attendees class module for the attendees"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+class Attendees(BaseModel, Base):
+    """This class defines the attendees model"""
+    __tablename__ = 'attendees'
+    attendee_name = Column(String(128), nullable=False)
+    attendee_email = Column(String(128), nullable=True)
+    attendee_phone = Column(String(128), nullable=False)
+    function_id = Column(Integer, ForeignKey('functions.function_id'))
+
+    functions = relationship("Functions", back_populates="attendees")
+    def __init__(self, *args, **kwargs):
+        """Initialization of the attendees model"""
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        """string represenation of an attendee"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
---
+++ Tana/models/tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Tasks class module for the tasks"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+class Tasks(BaseModel, Base):
+    """This class defines the tasks model"""
+    __tablename__ = 'tasks'
+    task_title = Column(String(128), nullable=False)
+    task_description = Column(String(128), nullable=False)
+    task_date = Column(String(128), nullable=False)
+    assigned_to = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    assigned_by = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    due_date = Column(Date, nullable=False)
+
+    assigned_to_user = relationship("users", foreign_keys=[assigned_to])
+    assigned_by_user = relationship("users", foreign_keys=[assigned_by])
+    def __init__(self, *args, **kwargs):
+        """Initialization of the tasks model"""
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+          """string represenation of a task"""
+          return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,                                      
+                                                 
+                                                 
+                                                 self.__dict__)
