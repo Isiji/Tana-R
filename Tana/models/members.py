416e05deca9ada8044d2bc1f3f44133762179f96
isiji <blairisiji@gmail.com>
2024-06-04 21:03:43 +0300

    making changes on tasks table

--- Tana/models/members.py
+++ Tana/models/members.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """users class module for the users"""
 from Tana.models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Index
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Enum
@@ -10,7 +10,7 @@ from flask_login import UserMixin
 class users(BaseModel, Base, UserMixin):
     """This class defines the users model"""
     __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True, index=True)
     name = Column(String(128), nullable=False)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
@@ -21,16 +21,13 @@ class users(BaseModel, Base, UserMixin):
 
     diaries = relationship("Diary", back_populates="user")
     human_resources = relationship("HumanResource", uselist=False, back_populates="user")
-    tasks_assigned = relationship("Tasks", back_populates="assigned_by", foreign_keys="Tasks.assigned_by")
-    tasks_assigned_to = relationship("Tasks", back_populates="assigned_to", foreign_keys="Tasks.assigned_to")
+    tasks_assigned = relationship("Tasks", back_populates="assigned_by_user", foreign_keys="Tasks.assigned_by")
+    tasks_assigned_to = relationship("Tasks", back_populates="assigned_to_user", foreign_keys="Tasks.assigned_to")
+
     def __init__(self, *args, **kwargs):
         """Initialization of the users model"""
         super().__init__(*args, **kwargs)
 
     def __str__(self):
-        """string represenation of a user"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
-    
-
-
+        """string representation of a user"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
--- Tana/models/tasks.py
+++ Tana/models/tasks.py
@@ -11,16 +11,17 @@ class Tasks(BaseModel, Base):
     task_title = Column(String(128), nullable=False)
     task_description = Column(String(128), nullable=False)
     task_date = Column(Date, nullable=False)
-    assigned_to = Column(Integer, ForeignKey('users.id'), nullable=False)
-    assigned_by = Column(Integer, ForeignKey('users.id'), nullable=False)
+    assigned_to = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    assigned_by = Column(Integer, ForeignKey('users.user_id'), nullable=False)
     due_date = Column(Date, nullable=False)
 
-    assigned_to_user = relationship("users", foreign_keys=[assigned_to])
-    assigned_by_user = relationship("users", foreign_keys=[assigned_by])
+    assigned_to_user = relationship("users", foreign_keys=[assigned_to], back_populates="tasks_assigned_to")
+    assigned_by_user = relationship("users", foreign_keys=[assigned_by], back_populates="tasks_assigned")
+
     def __init__(self, *args, **kwargs):
         """Initialization of the tasks model"""
         super().__init__(*args, **kwargs)
 
     def __str__(self):
-          """string represenation of a task"""
-          return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """string represenation of a task"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
