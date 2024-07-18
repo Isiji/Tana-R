8aa0b54a07c830599d8b6bc4861c4c6045ef904f
isiji <blairisiji@gmail.com>
2024-07-18 13:49:39 +0300

    dealing with reminders

--- Tana/reminder/routes.py
+++ Tana/reminder/routes.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-"""Routes for the reminders"""
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import current_user, login_required
 from Tana.models.reminder import Reminder
 from Tana.reminder.forms import ReminderForm
 from Tana import db_storage
@@ -8,8 +7,9 @@ from Tana import db_storage
 reminders = Blueprint('reminders', __name__)
 
 @reminders.route('/add_reminder', methods=['GET', 'POST'])
+@login_required
 def add_reminder():
-    """Route for the reminders page"""
+    """Route for adding a reminder"""
     form = ReminderForm()
     if form.validate_on_submit():
         reminder = Reminder(
@@ -18,12 +18,19 @@ def add_reminder():
             reminder_date=form.reminder_date.data,
             reminder_time=form.reminder_time.data,
             reminder_location=form.reminder_location.data,
-            reminder_contact=form.reminder_contact.data
+            reminder_contact=form.reminder_contact.data,
+            user_id=current_user.id  # Assign current user's ID to the reminder
         )
         db_storage.new(reminder)
         db_storage.save()
         flash('Reminder set successfully', 'success')
         return redirect(url_for('reminders.reminders_page'))
 
-    reminders = db_storage.all(Reminder).values()
-    return render_template('reminders.html', title='Reminders', form=form, reminders=reminders)
+    return render_template('reminders.html', title='Add Reminder', form=form)
+
+@reminders.route('/reminders')
+@login_required
+def reminders_page():
+    """Route for viewing reminders"""
+    reminders = db_storage.all(Reminder, filters={'user_id': current_user.id}, order_by={'reminder_date': 'desc', 'reminder_time': 'desc'})
+    return render_template('reminders.html', title='Reminders', reminders=reminders)
