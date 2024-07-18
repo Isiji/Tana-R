285d7f421d25e616da20ffb4f31f2d6b7b5a60dc
isiji <blairisiji@gmail.com>
2024-07-18 14:47:05 +0300

    reminder register succesfully

--- Tana/reminder/forms.py
+++ Tana/reminder/forms.py
@@ -1,12 +1,19 @@
-# forms.py
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, DateField, TimeField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, DateField, StringField, SubmitField, TimeField
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime
 
 class ReminderForm(FlaskForm):
     reminder_name = StringField('Reminder Name', validators=[DataRequired()])
     reminder_description = TextAreaField('Reminder Description', validators=[DataRequired()])
     reminder_date = DateField('Reminder Date', format='%Y-%m-%d', validators=[DataRequired()])
-    reminder_time = TimeField('Reminder Time', format='%H:%M:%S', validators=[DataRequired()])
+    reminder_time = TimeField('Reminder Time (24hr format)', format='%H:%M', validators=[DataRequired()])
     submit = SubmitField('Set Reminder')
+
+    def validate_reminder_date(self, reminder_date):
+        if reminder_date.data < datetime.now().date():
+            raise ValidationError('Reminder date must be in the future.')
+
+    def validate_reminder_time(self, reminder_time):
+        if reminder_time.data is None:
+            raise ValidationError('Reminder time is required.')
--- Tana/reminder/routes.py
+++ Tana/reminder/routes.py
@@ -1,10 +1,9 @@
-# routes.py
-
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import current_user, login_required
 from Tana.models.reminder import Reminder
 from Tana.reminder.forms import ReminderForm
 from Tana import db_storage
+from datetime import datetime
 
 reminders = Blueprint('reminders', __name__)
 
@@ -14,23 +13,21 @@ def add_reminder():
     """Route for adding a reminder"""
     form = ReminderForm()
     if form.validate_on_submit():
+        reminder_datetime = datetime.combine(form.reminder_date.data, form.reminder_time.data)
+        if reminder_datetime <= datetime.now():
+            flash('Reminder date and time must be in the future.', 'danger')
+            return redirect(url_for('reminders.add_reminder'))
+        
         reminder = Reminder(
             reminder_name=form.reminder_name.data,
             reminder_description=form.reminder_description.data,
             reminder_date=form.reminder_date.data,
             reminder_time=form.reminder_time.data,
-            user_id=current_user.id  # Assign current user's ID to the reminder
+            user_id=current_user.id
         )
         db_storage.new(reminder)
         db_storage.save()
         flash('Reminder set successfully', 'success')
-        return redirect(url_for('reminders.reminders_page'))
+        return redirect(url_for('reminders.add_reminder'))
 
-    return render_template('reminders.html', title='Add Reminder', form=form)
-
-@reminders.route('/view_reminders')
-@login_required
-def view_reminders():
-    """Route for viewing reminders"""
-    reminders = db_storage.all(Reminder, filters={'user_id': current_user.id}, order_by={'reminder_date': 'desc', 'reminder_time': 'desc'})
-    return render_template('reminders.html', title='Reminders', reminders=reminders)
+    return render_template('reminders.html', title='Reminders', form=form)
